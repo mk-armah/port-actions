1151db8d6fd9491eae5d73cf5beae02f54e5f4ff
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-02-08 22:58:00 +0000

    Update calculate_pr_metrics.py

--- calculate_pr_metrics.py
+++ calculate_pr_metrics.py
@@ -33,6 +33,7 @@ def calculate_pr_metrics():
 
     for pr in prs:
         if pr.created_at < start_date:
+            print(f"Time >> {created_at} < {start_date}")
             break  # Stop iterating once we are outside the time frame
 
         prs_opened += 1
