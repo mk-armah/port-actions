f40a3be5906c1302916a0275a233efdd52df97bc
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-04-18 08:57:35 +0000

    Update calculate_pr_metrics.py

--- src/calculate_pr_metrics.py
+++ src/calculate_pr_metrics.py
@@ -4,6 +4,7 @@ import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import json
 import logging
+import argparse
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
