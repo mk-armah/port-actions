25e9f628d5f399e3a6c541658d94217c5636b9ec
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-04-18 08:55:43 +0000

    Update calculate_pr_metrics.py

--- src/calculate_pr_metrics.py
+++ src/calculate_pr_metrics.py
@@ -163,4 +163,4 @@ if __name__ == "__main__":
 
     if args.platform == "github-actions":
         with open(os.getenv("GITHUB_ENV"), "a") as github_env:
-        github_env.write(f"metrics={metrics_json}\n")
+            github_env.write(f"metrics={metrics_json}\n")
