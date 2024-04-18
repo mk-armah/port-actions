23d033f798ed61db7f86243b0a95039df82541ea
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-04-18 07:07:10 +0000

    Update lead_time_for_changes.py

--- src/lead_time_for_changes.py
+++ src/lead_time_for_changes.py
@@ -26,7 +26,7 @@ class LeadTimeForChanges:
         self.branch = branch
         self.number_of_days = number_of_days
         self.commit_counting_method = commit_counting_method
-        self.github = Github(login_or_token = self.pat_token,seconds_between_requests=SECONDS_BETWEEN_REQUESTS, seconds_between_writes=SECONDS_BETWEEN_WRITES)
+        self.github = Github(login_or_token = pat_token,seconds_between_requests=SECONDS_BETWEEN_REQUESTS, seconds_between_writes=SECONDS_BETWEEN_WRITES)
         self.repo_object = self.github.get_repo(f"{self.owner}/{self.repo}")
 
     def __call__(self):
