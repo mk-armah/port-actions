dc3dfe4d16dd6596a41581bbc5873c7dbb54a723
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-06-12 07:38:25 +0000

    Update calculate_pr_metrics.py

--- src/calculate_pr_metrics.py
+++ src/calculate_pr_metrics.py
@@ -69,79 +69,112 @@ class RepositoryMetrics:
                     pr_metrics["time_to_approval"] = review.submitted_at - pr.created_at
 
         return pr_metrics
+        
+    def calculate_response_rate(self, prs, team_name):
+        total_requests = 0
+        responded_requests = 0
 
+        for pr in prs:
+            if pr.created_at >= self.start_date:
+                total_requests += 1
+                if pr.get_reviews().totalCount > 0:
+                    responded_requests += 1
+
+        response_rate = (responded_requests / total_requests) * 100 if total_requests else 0
+
+        return {f"{team_name}_response_rate": round(response_rate, 2)}
+
+    def calculate_response_time(self, prs, team_name):
+        total_response_time = datetime.timedelta(0)
+        total_responses = 0
+
+        for pr in prs:
+            if pr.created_at >= self.start_date:
+                reviews = pr.get_reviews()
+                for review in reviews:
+                    if review.user.login == team_name:
+                        response_time = review.submitted_at - pr.created_at
+                        total_response_time += response_time
+                        total_responses += 1
+                        break
+
+        average_response_time = self.timedelta_to_decimal_hours(
+            total_response_time / total_responses
+        ) if total_responses else 0
+
+        return {f"{team_name}_average_response_time": average_response_time}
+        
     def aggregate_results(self, results):
-        aggregated = {
-            "total_open_to_close_time": datetime.timedelta(0),
-            "total_time_to_first_review": datetime.timedelta(0),
-            "total_time_to_approval": datetime.timedelta(0),
-            "prs_opened": 0,
-            "prs_merged": 0,
-            "total_reviews": 0,
-            "total_commits": 0,
-            "total_loc_changed": 0,
-            "review_dates": [],
-        }
-
-        for result in results:
-            aggregated["total_open_to_close_time"] += result["open_to_close_time"]
-            aggregated["total_time_to_first_review"] += result["time_to_first_review"]
-            aggregated["total_time_to_approval"] += result["time_to_approval"]
-            aggregated["prs_opened"] += result["prs_opened"]
-            aggregated["prs_merged"] += result["prs_merged"]
-            aggregated["total_reviews"] += result["total_reviews"]
-            aggregated["total_commits"] += result["total_commits"]
-            aggregated["total_loc_changed"] += result["total_loc_changed"]
-            aggregated["review_dates"].extend(result["review_dates"])
-
-        # Calculate average PRs reviewed per week
-        review_weeks = {
-            review_date.isocalendar()[1] for review_date in aggregated["review_dates"]
-        }
-        average_prs_reviewed_per_week = len(review_weeks) / max(1, self.timeframe)
-
-        metrics = {
-            "id": self.repo.id,
-            "average_open_to_close_time": self.timedelta_to_decimal_hours(
-                aggregated["total_open_to_close_time"] / aggregated["prs_merged"]
-            )
-            if aggregated["prs_merged"]
-            else 0,
-            "average_time_to_first_review": self.timedelta_to_decimal_hours(
-                aggregated["total_time_to_first_review"] / aggregated["prs_opened"]
-            )
-            if aggregated["prs_opened"]
-            else 0,
-            "average_time_to_approval": self.timedelta_to_decimal_hours(
-                aggregated["total_time_to_approval"] / aggregated["prs_opened"]
-            )
-            if aggregated["prs_opened"]
-            else 0,
-            "prs_opened": aggregated["prs_opened"],
-            "weekly_prs_merged": self.timedelta_to_decimal_hours(
-                aggregated["total_open_to_close_time"] / max(1, self.timeframe)
-            )
-            if aggregated["prs_merged"]
-            else 0,
-            "average_reviews_per_pr": round(
-                aggregated["total_reviews"] / aggregated["prs_opened"], 2
-            )
-            if aggregated["prs_opened"]
-            else 0,
-            "average_commits_per_pr": round(
-                aggregated["total_commits"] / aggregated["prs_opened"], 2
-            )
-            if aggregated["prs_opened"]
-            else 0,
-            "average_loc_changed_per_pr": round(
-                aggregated["total_loc_changed"] / aggregated["prs_opened"], 2
-            )
-            if aggregated["prs_opened"]
-            else 0,
-            "average_prs_reviewed_per_week": round(average_prs_reviewed_per_week, 2),
-        }
-
-        return metrics
+            aggregated = {
+                "total_open_to_close_time": datetime.timedelta(0),
+                "total_time_to_first_review": datetime.timedelta(0),
+                "total_time_to_approval": datetime.timedelta(0),
+                "prs_opened": 0,
+                "prs_merged": 0,
+                "total_reviews": 0,
+                "total_commits": 0,
+                "total_loc_changed": 0,
+                "review_dates": [],
+            }
+    
+            for result in results:
+                aggregated["total_open_to_close_time"] += result["open_to_close_time"]
+                aggregated["total_time_to_first_review"] += result["time_to_first_review"]
+                aggregated["total_time_to_approval"] += result["time_to_approval"]
+                aggregated["prs_opened"] += result["prs_opened"]
+                aggregated["prs_merged"] += result["prs_merged"]
+                aggregated["total_reviews"] += result["total_reviews"]
+                aggregated["total_commits"] += result["total_commits"]
+                aggregated["total_loc_changed"] += result["total_loc_changed"]
+                aggregated["review_dates"].extend(result["review_dates"])
+    
+            # Calculate average PRs reviewed per week
+            review_weeks = {
+                review_date.isocalendar()[1] for review_date in aggregated["review_dates"]
+            }
+            average_prs_reviewed_per_week = len(review_weeks) / max(1, self.timeframe / 7)
+    
+            metrics = {
+                "id": self.repo.id,
+                "average_open_to_close_time": self.timedelta_to_decimal_hours(
+                    aggregated["total_open_to_close_time"] / aggregated["prs_merged"]
+                )
+                if aggregated["prs_merged"]
+                else 0,
+                "average_time_to_first_review": self.timedelta_to_decimal_hours(
+                    aggregated["total_time_to_first_review"] / aggregated["prs_opened"]
+                )
+                if aggregated["prs_opened"]
+                else 0,
+                "average_time_to_approval": self.timedelta_to_decimal_hours(
+                    aggregated["total_time_to_approval"] / aggregated["prs_opened"]
+                )
+                if aggregated["prs_opened"]
+                else 0,
+                "prs_opened": aggregated["prs_opened"],
+                "weekly_prs_merged": self.timedelta_to_decimal_hours(
+                    aggregated["total_open_to_close_time"] / max(1, self.timeframe / 7)
+                )
+                if aggregated["prs_merged"]
+                else 0,
+                "average_reviews_per_pr": round(
+                    aggregated["total_reviews"] / aggregated["prs_opened"], 2
+                )
+                if aggregated["prs_opened"]
+                else 0,
+                "average_commits_per_pr": round(
+                    aggregated["total_commits"] / aggregated["prs_opened"], 2
+                )
+                if aggregated["prs_opened"]
+                else 0,
+                "average_loc_changed_per_pr": round(
+                    aggregated["total_loc_changed"] / aggregated["prs_opened"], 2
+                )
+                if aggregated["prs_opened"]
+                else 0,
+                "average_prs_reviewed_per_week": round(average_prs_reviewed_per_week, 2),
+            }
+            return metrics
 
     def timedelta_to_decimal_hours(self, td):
         return round(td.total_seconds() / 3600, 2)
