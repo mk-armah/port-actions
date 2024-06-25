f21c83f6eddb0872648eb197fbeec7938ab3f530
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-06-25 20:53:15 +0000

    Update calculate_team_metrics.py

--- src/calculate_team_metrics.py
+++ src/calculate_team_metrics.py
@@ -1,5 +1,5 @@
 import os
-from github import Github, PullRequest, Team, NamedUser
+from github import Github, Team, PullRequest,GithubException
 import datetime
 import json
 import logging
@@ -16,10 +16,18 @@ logging.basicConfig(
 
 class TeamMetrics:
     def __init__(
-        self, owner: str, repo: str, timeframe: int, team_name: str, pat_token: str
+        self, owner: str, repo: str, timeframe: int, team_name: str, pat_token: str, base_url: str | None
     ) -> None:
-        self.github_client = Github(pat_token)
-        self.repo = self.github_client.get_repo(f"{owner}/{repo}")
+        try:
+            self.github_client = Github(login_or_token=pat_token, base_url=base_url) if base_url else Github(pat_token)
+            self.repo = self.github_client.get_repo(f"{owner}/{repo}")
+        except GithubException as e:
+            logging.error(f"Failed to initialize GitHub client: {e}")
+            raise
+        except Exception as e:
+            logging.error(f"Unexpected error during initialization: {e} - verify that your github credentials are valid")
+            raise
+
         self.team_slug = self.convert_to_slug(team_name)
         self.timeframe = timeframe
         self.start_date = datetime.datetime.now(
@@ -34,42 +42,64 @@ class TeamMetrics:
         return re.sub(r"\s+", "-", name.strip()).lower()
 
     def get_team_members(self) -> List[str]:
-        logging.info(f"Fetching team members for {self.team_slug}")
-        team = self.github_client.get_organization(
-            self.repo.owner.login
-        ).get_team_by_slug(self.team_slug)
-        return [member.login for member in team.get_members()]
+        try:
+            logging.info(f"Fetching team members for {self.team_slug}")
+            team = self.github_client.get_organization(
+                self.repo.owner.login
+            ).get_team_by_slug(self.team_slug)
+            return [member.login for member in team.get_members()]
+        
+        except GithubException as e:
+            logging.error(f"Failed to fetch team members: {e}")
+            raise
+        except Exception as e:
+            logging.error(f"Unexpected error while fetching team members: {e}")
+            raise
 
     def calculate_metrics(self) -> Dict[str, Any]:
-        prs = self.repo.get_pulls(state="all", sort="created", direction="desc")
-        filtered_prs: List[PullRequest.PullRequest] = [
-            pr for pr in prs if pr.created_at >= self.start_date
-        ]
-        logging.info(
-            f"Fetched {len(filtered_prs)} pull requests for the specified timeframe"
-        )
+        try:
+            prs = self.repo.get_pulls(state="all", sort="created", direction="desc")
+            filtered_prs: List[PullRequest.PullRequest] = [
+                pr for pr in prs if pr.created_at >= self.start_date
+            ]
+            logging.info(
+                f"Fetched {len(filtered_prs)} pull requests for the specified timeframe"
+            )
 
-        logging.info(f"Fetching team info for {self.repo.full_name}/{self.team_slug}")
-        response_rate, response_time = self.calculate_response_metrics(filtered_prs)
-        team_info = self.get_team_info()
-        return {**response_rate, **response_time, **team_info}
+            logging.info(f"Fetching team info for {self.repo.full_name}/{self.team_slug}")
+            response_rate, response_time = self.calculate_response_metrics(filtered_prs)
+            team_info = self.get_team_info()
+            return {**response_rate, **response_time, **team_info}
+        except GithubException as e:
+            logging.error(f"Failed to calculate metrics: {e}")
+            raise
+        except Exception as e:
+            logging.error(f"Unexpected error while calculating metrics: {e}")
+            raise
 
     def get_team_info(self) -> Dict[str, Any]:
-        logging.info(f"Fetching team info from {self.repo.owner.login} organization")
-        team = self.github_client.get_organization(
-            self.repo.owner.login
-        ).get_team_by_slug(self.team_slug)
-        return {
-            "id": team.id,
-            "name": team.name,
-            "description": team.description,
-            "members_count": team.members_count,
-            "repos_count": team.repos_count,
-            "slug": team.slug,
-            "link": team.html_url,
-            "permission": team.permission,
-            "notification_setting": team.notification_setting,
-        }
+        try:
+            logging.info(f"Fetching team info from {self.repo.owner.login} organization")
+            team:Team.Team = self.github_client.get_organization(
+                self.repo.owner.login
+            ).get_team_by_slug(self.team_slug)
+            return {
+                "id": team.id,
+                "name": team.name,
+                "description": team.description,
+                "members_count": team.members_count,
+                "repos_count": team.repos_count,
+                "slug": team.slug,
+                "link": team.html_url,
+                "permission": team.permission,
+                "notification_setting": team.notification_setting,
+            }
+        except GithubException as e:
+            logging.error(f"Failed to fetch team info: {e}")
+            raise
+        except Exception as e:
+            logging.error(f"Unexpected error while fetching team info: {e}")
+            raise
 
     def calculate_response_metrics(
         self, prs: List[PullRequest.PullRequest]
@@ -82,23 +112,34 @@ class TeamMetrics:
 
         def fetch_reviews(pr: PullRequest.PullRequest) -> None:
             nonlocal responded_requests, total_response_time, total_responses, total_requests
-            with self.semaphore:  # Ensure limited concurrent requests
-                # Check if the team was explicitly requested to review the PR
-                if any(team.slug == self.team_slug for team in pr.requested_teams):
-                    total_requests += 1
-                    reviews = pr.get_reviews()
-                    for review in reviews:
-                        if review.user.login in self.team_members:
-                            responded_requests += 1
-                            response_time = review.submitted_at - pr.created_at
-                            total_response_time += response_time
-                            total_responses += 1
-                            break
+            try:
+                with self.semaphore:  # Ensure limited concurrent requests
+                    # Check if the team was explicitly requested to review the PR
+                    if any(team.slug == self.team_slug for team in pr.requested_teams):
+                        total_requests += 1
+                        reviews = pr.get_reviews()
+                        for review in reviews:
+                            if review.user.login in self.team_members:
+                                responded_requests += 1
+                                response_time = review.submitted_at - pr.created_at
+                                total_response_time += response_time
+                                total_responses += 1
+                                break
+            except GithubException as e:
+                logging.error(f"Failed to fetch reviews for PR {pr.number}: {e}")
+            except Exception as e:
+                logging.error(f"Unexpected error while fetching reviews for PR {pr.number}: {e}")
 
-        with ThreadPoolExecutor(max_workers=10) as executor:
-            future_to_pr = {executor.submit(fetch_reviews, pr): pr for pr in prs}
-            for future in as_completed(future_to_pr):
-                pass
+        try:
+
+            with ThreadPoolExecutor(max_workers=10) as executor:
+                future_to_pr = {executor.submit(fetch_reviews, pr): pr for pr in prs}
+                for future in as_completed(future_to_pr):
+                    pass
+        
+        except Exception as e:
+            logging.error(f"Unexpected error in ThreadPoolExecutor: {e}")
+            raise
 
         response_rate = (
             (responded_requests / total_requests) * 100 if total_requests else 0
@@ -140,21 +181,31 @@ if __name__ == "__main__":
         choices=["github-actions", "self-hosted"],
         help="CI/CD platform type",
     )
+    parser.add_argument(
+        "--base-url",
+        help="Base URL for self-hosted GitHub instance (e.g., https://github.example.com/api/v3)",
+        default= None
+    )
+
     args = parser.parse_args()
 
-    logging.info(f"Repository Name: {args.owner}/{args.repo}")
-    logging.info(f"TimeFrame (in days): {args.timeframe}")
-    logging.info(f"Team Name: {args.team}")
+    try:
+        logging.info(f"Repository Name: {args.owner}/{args.repo}")
+        logging.info(f"TimeFrame (in days): {args.timeframe}")
+        logging.info(f"Team Name: {args.team}")
+        
+        team_metrics = TeamMetrics(
+            args.owner, args.repo, args.timeframe, args.team, pat_token=args.token, base_url= args.base_url
+        )
+        metrics = team_metrics.calculate_metrics()
+        metrics_json = json.dumps(metrics, default=str)
+        logging.info(f"Team info: {metrics_json}")
 
-    team_metrics = TeamMetrics(
-        args.owner, args.repo, args.timeframe, args.team, pat_token=args.token
-    )
-    metrics = team_metrics.calculate_metrics()
-    metrics_json = json.dumps(metrics, default=str)
-    logging.info(f"Team info: {metrics_json}")
+        if args.platform == "github-actions":
+            with open(os.getenv("GITHUB_ENV", ""), "a") as github_env:
+                github_env.write(f"team_metrics={metrics_json}\n")
 
-    if args.platform == "github-actions":
-        with open(os.getenv("GITHUB_ENV", ""), "a") as github_env:
-            github_env.write(f"team_metrics={metrics_json}\n")
+        logging.info(f"Execution Time: {time.time() - start_time}")
 
-    logging.info(f"Execution Time: {time.time() - start_time}")
+    except Exception as e:
+        logging.error(f"Failed to execute script: {e}")
