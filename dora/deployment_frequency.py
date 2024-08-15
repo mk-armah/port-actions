60970c1ab3dcb5bcd546908a94f7de9ca870b102
Michael Kofi Armah <mikeyarmah@gmail.com>
2024-08-15 14:33:34 +0000

    Create deployment_frequency.py

---
+++ dora/deployment_frequency.py
@@ -0,0 +1,124 @@
+import datetime
+import os
+import json
+from github import Github
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+class DeploymentFrequency:
+    def __init__(self, owner, repo, workflows, branch, number_of_days, token, github_host):
+        self.owner, self.repo = owner, repo
+        self.branch = branch
+        self.number_of_days = number_of_days
+        self.token = token
+        try:
+            self.github = (
+                Github(login_or_token=token, base_url=github_host)
+                if github_host
+                else Github(token)
+            )
+            self.owner = owner
+        except GithubException as e:
+            logging.error(f"Failed to initialize GitHub client: {e}")
+            raise
+        except Exception as e:
+            logging.error(
+                f"Unexpected error during initialization: {e} - verify that your github credentials are valid"
+            )
+            raise
+        self.repo_object = self.github.get_repo(f"{self.owner}/{self.repo}")
+        try:
+            self.workflows = json.loads(workflows)
+        except JSONDecodeError:
+            logging.error("Invalid JSON format for workflows. Using an empty list.")
+            self.workflows = []
+
+    def get_workflows(self):
+        if not self.workflows:
+            workflows = self.repo_object.get_workflows()
+            workflow_ids = [workflow.id for workflow in workflows]
+            logging.info(f"Found {len(workflow_ids)} workflows in Repo")
+        else:
+            workflow_ids = self.workflows
+            logging.info(f"Workflows: {workflow_ids}")
+        return workflow_ids
+
+    def fetch_workflow_runs(self):
+        workflow_ids = self.get_workflows()
+        workflow_runs_list = []
+        unique_dates = set()
+        for workflow_id in workflow_ids:
+            for run in self.repo_object.get_workflow(workflow_id).get_runs():
+                run_date = run.created_at.replace(tzinfo=None)
+                if run.head_branch == self.branch and run_date > datetime.datetime.now() - datetime.timedelta(days=self.number_of_days):
+                    workflow_runs_list.append(run)
+                    unique_dates.add(run_date.date())
+        return workflow_runs_list, unique_dates
+
+    def calculate_deployments_per_day(self, workflow_runs_list):
+        if self.number_of_days > 0:
+            return len(workflow_runs_list) / self.number_of_days
+        return 0
+
+    def compute_rating(self, deployments_per_day):
+        daily_deployment = 1
+        weekly_deployment = 1 / 7
+        monthly_deployment = 1 / 30
+        yearly_deployment = 1 / 365
+
+        if deployments_per_day > daily_deployment:
+            return "Elite", "brightgreen"
+        elif weekly_deployment <= deployments_per_day <= daily_deployment:
+            return "High", "green"
+        elif monthly_deployment <= deployments_per_day < weekly_deployment:
+            return "Medium", "yellow"
+        elif yearly_deployment < deployments_per_day < monthly_deployment:
+            return "Low", "red"
+        else:
+            return "None", "lightgrey"
+
+    def __call__(self):
+        workflow_runs_list, unique_dates = self.fetch_workflow_runs()
+        deployments_per_day = self.calculate_deployments_per_day(workflow_runs_list)
+        rating, color = self.compute_rating(deployments_per_day)
+
+        logging.info(f"Owner/Repo: {self.owner}/{self.repo}")
+        logging.info(f"Branch: {self.branch}")
+        logging.info(f"Number of days: {self.number_of_days}")
+        logging.info(f"Deployment frequency over the last {self.number_of_days} days is {deployments_per_day} per day")
+        logging.info(f"Rating: {rating} ({color})")
+
+        return json.dumps({
+            "deployment_frequency": round(deployments_per_day, 2),
+            "rating": rating,
+            "number_of_unique_deployment_days": len(unique_dates),
+            "number_of_unique_deployment_weeks": len({date.isocalendar()[1] for date in unique_dates}),
+            "number_of_unique_deployment_months": len({date.month for date in unique_dates}),
+            "total_deployments": len(workflow_runs_list),
+        }, default=str)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Calculate Deployment Frequency.')
+    parser.add_argument('--owner', required=True, help='Owner of the repository')
+    parser.add_argument('--repo', required=True, help='Repository name')
+    parser.add_argument('--token', required=True, help='GitHub token')
+    parser.add_argument(
+            "--github-host",
+            help="Base URL for self-hosted GitHub instance (e.g., https://api.example-github.com)",
+            default=None,
+        )
+    parser.add_argument('--workflows', required=True, help='GitHub workflows as a JSON string.')
+    parser.add_argument('--branch', default='main', help='Branch name')
+    parser.add_argument('--time-frame', type=int, default=30, help='Time Frame in days')
+    parser.add_argument('--platform', default='github-actions', choices=['github-actions', 'self-hosted'], help='CI/CD platform type')
+    args = parser.parse_args()
+
+    deployment_frequency = DeploymentFrequency(args.owner, args.repo, args.workflows, args.branch, args.time_frame, token = args.token, github_host = args.github_host)
+    report = deployment_frequency()
+    print(report)
+    
+    if args.platform == "github-actions":
+       with open(os.getenv("GITHUB_ENV"), "a") as github_env:
+           github_env.write(f"deployment_frequency_report={report}\n")
